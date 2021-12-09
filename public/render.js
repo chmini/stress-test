88bd027a464727e2e22608af6831acb032a71de8
chmini <beelzebule@naver.com>
2021-12-09 16:31:41 +0900

    check form validation

--- public/render.js
+++ public/render.js
@@ -1,9 +1,16 @@
 const $form = document.querySelector('.question-form');
+const $result = document.querySelector('.result');
 
 // fetch
-const render = async () => {
-  const { db } = await fetch('/db').then(data => data.json());
+let db = {};
 
+const fetchDB = async () => {
+  const { db } = await fetch('/db').then(data => data.json());
+  return db;
+};
+
+const render = async () => {
+  db = await fetchDB();
   $form.innerHTML = `${db.stressTestQuestions
     .map(
       ({ id, content, order }) => `
@@ -24,3 +31,22 @@ const render = async () => {
 };
 
 document.addEventListener('DOMContentLoaded', render);
+
+$form.onsubmit = async e => {
+  e.preventDefault();
+
+  const formData = [...new FormData($form)];
+
+  if (formData.length < db.stressTestQuestions.length) return;
+
+  const data = await fetch('/result', {
+    method: 'POST',
+    headers: { 'content-type': 'application/json' },
+    body: JSON.stringify(formData.map(data => +data[1])),
+  }).then(data => data.json());
+
+  $form.style.display = 'none';
+  // $result.style.display = 'block'
+
+  $result.innerHTML = `<div>점수 : ${data.result}</div><div>${data.message}</div>`;
+};
